d66738716ebd6ef5ba3dda17cdd14b264fcdf93b
fangzhou <westark@qq.com>
2022-06-03 22:18:54 +0800

    feat(vueSource): add vite

--- playground/ts/rollup.config.js
+++ playground/ts/rollup.config.js
@@ -1,7 +1,8 @@
 import path from "path"
 import { babel } from "@rollup/plugin-babel"
 import nodeResolve from "@rollup/plugin-node-resolve"
-import replace from '@rollup/plugin-replace'
+import replace from "@rollup/plugin-replace"
+import server from "rollup-plugin-server"
 
 const resolve = function (...args) {
   return path.resolve(__dirname, ...args)
@@ -23,8 +24,15 @@ module.exports = {
       exclude: "node_modules/**",
       extensions,
     }),
-		replace({
-			"process.env.NODE_ENV": false
-		})
+    replace({
+      "process.env.NODE_ENV": false,
+    }),
+
+    server({
+      open: true,
+      verbose: true,
+      host: "localhost",
+      port: 6667,
+    }),
   ],
 }
